86cb49a440817db956396deeadcfa3ba95700b58
Matteo Yuen <matteo.yuen@gmail.com>
2024-12-12 09:49:58 -0600

    pc push

---
+++ OpenCV/build/intermediates/cxx/RelWithDebInfo/773581g3/logs/armeabi-v7a/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\matte\\AppData\\Local\\Android\\sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\matte\\AndroidStudioProjects\\livefeed\\OpenCV\\.cxx\\RelWithDebInfo\\773581g3\\armeabi-v7a" ^
+  opencv_jni_shared
